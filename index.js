bb69b2a984af1affdc2ff61db17b565b247f7ff1
tal8374 <anatoly.i@reflectiz.com>
2020-04-28 16:11:23 +0300

    Adding Procfile

---
+++ index.js
@@ -0,0 +1,34 @@
+var PORT = process.env.PORT || 5000;
+var express = require('express');
+var app = express();
+
+const createError = require('http-errors');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+const config = require('./config');
+
+const formRouter = require('./routes/form.route');
+
+var http = require('http');
+var server = http.Server(app);
+
+app.use(express.static('client'));
+
+server.listen(PORT, function() {
+  console.log('Chat server running');
+});
+
+mongoose.connect(config.mongo_url, {}, function (err) {
+    if (err) console.log(err);
+});
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
+app.use('/form', formRouter);
+
